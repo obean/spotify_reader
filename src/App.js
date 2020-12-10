a193e379d1f0b520dbd30ce5ef029877bca22c86
ollie beney <ombeney@gmail.com>
2020-12-10 19:27:10 +0000

    takes you to spotify to login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
+import React, { useState, useEffect, Component } from "react";
+//import hash from "./hash";
 import logo from './logo.svg';
 import './App.css';
 
+export const authEndpoint = 'https://accounts.spotify.com/authorize?'
+const clientId = process.env.REACT_APP_CLIENTID;
+const clientSecret = process.env.REACT_APP_CLIENTSECRET;
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+
+const hash = window.location.hash
+  .substring(1)
+  .split("&")
+  .reduce(function(initial, item) {
+    if (item) {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+
+
 function App() {
+  const [token, setToken] = useState(false)
+    useEffect(() => {
+        let _token = hash.access_token;
+        if(_token) {
+          setToken(_token)     
+    }
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {!token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}client_id=${process.env.REACT_APP_CLIENTID}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+          > Login to Spotify</a>  
+        )}
+        {/* {token &&(
+        // player goes here
+        )} */}
         <a
           className="App-link"
           href="https://reactjs.org"
